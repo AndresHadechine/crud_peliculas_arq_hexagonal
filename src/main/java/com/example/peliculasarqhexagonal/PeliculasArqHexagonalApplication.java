4d8ef3e861668ddc136337b799ddd20eb0732540
AndresHadechine <andrhali@suramericana.com.co>
2023-02-07 15:08:21 -0500

    se crea proyecto y su escrutura incial

---
+++ src/main/java/com/example/peliculasarqhexagonal/PeliculasArqHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.peliculasarqhexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasArqHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeliculasArqHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/peliculasarqhexagonal/PeliculasArqHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peliculasarqhexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasArqHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
